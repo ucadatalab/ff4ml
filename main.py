ec98d5c61d1a6506c4b8668be42001384f8df9e4
Ignacio Díaz Cano <ignacio.diaz@uca.es>
2019-11-28 13:26:58 +0100

    update parameters

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def getArguments():
     parser.add_argument('model', metavar='MODELs', help='ML model (svc,rf,lr)', choices=['svc','rf','lr'])
     parser.add_argument('rep', metavar='REPETITIONs', help='Repetition number (1-20).', type=int)
     parser.add_argument('kfold', metavar='K-FOLDs', help='Kfold number (1-5).',type=int)
- #   parser.add_argument('exec_ts', metavar='Timestamp', help='Timestamp.') # Ejecución en supercomputador
+    parser.add_argument('exec_ts', metavar='Timestamp', help='Timestamp.') # Ejecución en supercomputador
     args = parser.parse_args()
     return args
 
@@ -75,15 +75,13 @@ def main(args):
     model=args.model
     rep=args.rep
     kfold=args.kfold
-#    ts=args.exec_ts  # Ejecución en supercomputador
+    ts=args.exec_ts  # Ejecución en supercomputador
 
     instantIni = datetime.now()
 
- #   root_path = '../data/'
- #   root_path_output = '../results/' + str(ts) + '/'
-    root_path = './data/'
-    root_path_output = './results/'
-
+    root_path = '../data/'
+    root_path_output = '../results/' + str(ts) + '/'
+    
     mc_file = 'ugr16_multiclass.csv'
     mcfold_file = 'ugr16_multiclass_folds.csv'
     mcvars_file = 'ugr16_multiclass_folds_selecvars.csv'
