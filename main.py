0c8a93a669ee7ed6bce365dfaa82405b47a7265f
Ignacio Díaz Cano <ignacio.diaz@uca.es>
2019-12-08 20:35:08 +0100

    SVC Fixed

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def getArguments():
     parser.add_argument('model', metavar='MODELs', help='ML model (svc,rf,lr)', choices=['svc','rf','lr'])
     parser.add_argument('rep', metavar='REPETITIONs', help='Repetition number (1-20).', type=int)
     parser.add_argument('kfold', metavar='K-FOLDs', help='Kfold number (1-5).',type=int)
-    parser.add_argument('exec_ts', metavar='Timestamp', help='Timestamp.') # Ejecución en supercomputador
+   # parser.add_argument('exec_ts', metavar='Timestamp', help='Timestamp.') # Ejecución en supercomputador
 
     return parser.parse_args()
 
@@ -80,14 +80,14 @@ def main(args):
     model=args.model
     rep=args.rep
     kfold=args.kfold
-    ts=args.exec_ts  # Ejecución en supercomputador
+  #  ts=args.exec_ts  # Ejecución en supercomputador
 
     instantIni = datetime.now()
 
-#   root_path = './data/'
-#   root_path_output = './results/'
-    root_path = '../data/' # Ejecución en supercomputador
-    root_path_output = '../results/' + str(ts) + '/' # Ejecución en supercomputador
+    root_path = './data/'
+    root_path_output = './results/'
+  #  root_path = '../data/' # Ejecución en supercomputador
+   # root_path_output = '../results/' + str(ts) + '/' # Ejecución en supercomputador
     
     mc_file = 'ugr16_multiclass.csv'
     mcfold_file = 'ugr16_multiclass_folds.csv'
@@ -198,7 +198,7 @@ def main(args):
         tmodel = LogisticRegression(random_state=0, penalty='none', multi_class='auto',
                                     solver='lbfgs', verbose=verbose)
     elif model == 'svc':
-        cs = int(bp.get('C'))
+        cs = float(bp.get('C'))
         print("cs: ", cs)
         ga = float(bp.get('gamma'))
         print("ga: ", ga)
