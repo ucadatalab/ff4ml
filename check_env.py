71cce4efa246b5055146c72f61abf657f15a0740
Roberto Magán <robertomagan@gmail.com>
2021-02-24 13:08:00 +0100

    Script for checking environment dependencies

---
+++ check_env.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import sklearn as sk
+import skopt
+
+print("scikit-learn version: {0}".format(sk.__version__))
+print("pandas version: {0}".format(pd.__version__))
+print("scikit-optimize version: {0}".format(skopt.__version__))
