cb336b146517ac0993a379f940c5234ff0b8614a
ROBERTO MAGAN CARRION <u05679288@cai3.uca.es>
2021-02-24 13:18:21 +0100

    Adding numpy version checking

--- check_env.py
+++ check_env.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import numpy as np
 import sklearn as sk
 import skopt
 
 print("scikit-learn version: {0}".format(sk.__version__))
 print("pandas version: {0}".format(pd.__version__))
 print("scikit-optimize version: {0}".format(skopt.__version__))
+print("numpy version: {0}".format(np.__version__))
+
